3c5a3da8b6faf93071f2616cf3dc95cf51b75165
James Dennis <jdennis@gmail.com>
2009-10-07 15:40:36 -0400

    Added distutils support

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+ 
+from distutils.core import setup
+ 
+setup(name='wikipydia',
+      version='0.1',
+      description='Python Client Library for the Wikipedia API',
+      author='James Dennis',
+      author_email='jdennis@gmail.com',
+      url='http://github.com/j2labs/wikipydia',
+      packages=['wikipydia'])
